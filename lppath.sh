fd0d8fd4cd3688057953829aa2855a38f7a8bdfd
ANewJanuary <artin2007majidi@gmail.com>
2025-10-03 12:01:44 -0400

    echo was printing newlines that annoyed me

--- lppath.sh
+++ lppath.sh
@@ -4,15 +4,17 @@ if [[ $1 == "n" ]]; then
 				select=$(find . -print | fzf)
 				ext=$(basename "$select")
 				name=$(echo "${ext%.*}")
-				echo -e "[[$name]]" | wl-copy
+				echo -e -n "[[$name]]" | wl-copy
+
 elif [[ $1 == "p" ]]; then
 				cd ~/Vshrd/Learner-Portfolio
 				select=$(find . -print | fzf)
 				ext=$(basename "$select")
 				name=$(echo "${ext%.*}")
-				echo -e "[$name]: $select" | wl-copy
+				echo -e -n "[$name]: $select" | wl-copy
 else
 				echo "Arguments incorrect. \
 								 Use lpc n to copy the name and \
 								 lpc p to copy the path."
 fi
+
